953bd4bebd2a559bd81313d2c16652412d8cc6be
Manikprabhu1999 <mbjewargi@gmail.com>
2022-11-01 00:58:40 +0530

    Added:First Program Added

---
+++ src/com/bridgelabz/SnakeLadder.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class SnakeLadder {
+public static void main(String[] args) {
+	System.out.println("Snake and Ladder game played with player at position zero");
+}
+}
