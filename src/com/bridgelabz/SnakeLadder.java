af6e36d4b995295f0733a218d3fcba7c025736e7
Manikprabhu1999 <mbjewargi@gmail.com>
2022-11-01 01:14:59 +0530

    Added:Rolling of Dice

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -1,7 +1,16 @@
 package com.bridgelabz;
 
 public class SnakeLadder {
+public void SnakeLadder() {
+System.out.println("the player rolls dice from number 1 to 6:");
+	int diceRoll=(int)(Math.random()*6+1);
+	
+	System.out.println("The Dice is rolled "+diceRoll+" No of Times");
+}
 public static void main(String[] args) {
-	System.out.println("Snake and Ladder game played with player at position zero");
+	SnakeLadder obj=new SnakeLadder();
+	obj.SnakeLadder();
 }
+	
 }
+
