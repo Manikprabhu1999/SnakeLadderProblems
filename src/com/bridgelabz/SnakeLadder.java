549213f31060effcc3b0498b74128cac89565267
Manikprabhu1999 <mbjewargi@gmail.com>
2022-11-01 14:47:05 +0530

    Added:Rolling a DICE

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -1,16 +1,32 @@
 package com.bridgelabz;
-
+import java.util.Scanner;
 public class SnakeLadder {
 public void SnakeLadder() {
-System.out.println("the player rolls dice from number 1 to 6:");
-	int diceRoll=(int)(Math.random()*6+1);
-	
-	System.out.println("The Dice is rolled "+diceRoll+" No of Times");
-}
-public static void main(String[] args) {
-	SnakeLadder obj=new SnakeLadder();
-	obj.SnakeLadder();
-}
-	
-}
+	Scanner scanner=new Scanner(System.in);
+	System.out.println("Enter 1:No Play");
+	System.out.println("Enter 2:Player Climbs Ladder");
+	System.out.println("Enter 3:Player falls from Ladder" );
+	int condition=scanner.nextInt();
+	   int diceRoll =(int) (Math.random()*6+1);
+	  System.out.println("The Dice is Rolled:"+ diceRoll);
+	int position=0;
+	switch(condition)
+	{
+	case 1:
+		    System.out.println("The player Will be at the Same Position (No Play):"+ position);
+		   break;
+	case 2:
+		    System.out.println("The player climbs the Ladder:" + diceRoll + position);
+		   break;
+	case 3:
+		    System.out.println("The player fall from Ladder:" + diceRoll +position);
+	}
 
+		}
+		
+		public static void main(String[] args) {
+			SnakeLadder obj=new SnakeLadder();
+			obj.SnakeLadder();
+		}
+		
+	}
---
+++ src/com/bridgelabz/UC3.java
@@ -0,0 +1,35 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class UC3 {
+
+	public void SnakeLadder() {
+Scanner scanner=new Scanner(System.in);
+System.out.println("Enter 1:No Play");
+System.out.println("Enter 2:Player Climbs Ladder");
+System.out.println("Enter 3:Player falls from Ladder" );
+int condition=scanner.nextInt();
+   int diceRoll =(int) (Math.random()*6+1);
+  System.out.println("The Dice is Rolled:"+ diceRoll);
+int position=0;
+switch(condition)
+{
+case 1:
+	    System.out.println("The player Will be at the Same Position (No Play):"+ position);
+	   break;
+case 2:
+	    System.out.println("The player climbs the Ladder:" + diceRoll + position);
+	   break;
+case 3:
+	    System.out.println("The player fall from Ladder:" + diceRoll +position);
+}
+
+	}
+	
+	public static void main(String[] args) {
+		UC3 obj=new UC3();
+		obj.SnakeLadder();
+	}
+	
+}
